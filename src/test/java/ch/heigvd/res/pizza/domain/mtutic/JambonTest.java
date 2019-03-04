9c5b12b930743bf5b6a71857ed1e1938aa291531
Mateo Tutic <mateo@tutic.ch>
2019-03-04 10:34:09 +0100

    Add jambon with tests

---
+++ src/test/java/ch/heigvd/res/pizza/domain/mtutic/JambonTest.java
@@ -0,0 +1,32 @@
+package ch.heigvd.res.pizza.domain.mtutic;
+
+import ch.heigvd.res.pizza.domain.Pizzaiolo;
+import ch.heigvd.res.pizza.protocol.OrderRequest;
+import ch.heigvd.res.pizza.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JambonTest {
+
+  @Test
+  void thePriceAndNameForJambonShouldBeCorrect() {
+    Jambon beer = new Jambon();
+    assertEquals(beer.getName(), Jambon.NAME);
+    assertEquals(beer.getPrice(), Jambon.PRICE);
+  }
+
+  @Test
+  void aPizzaioloShouldAcceptAnOrderForJambon() {
+    Pizzaiolo mario = new Pizzaiolo();
+    String productName = "ch.heigvd.res.pizza.domain.mtutic.Jambon";
+    int numberOfPizzas = 2;
+    OrderRequest request = new OrderRequest(numberOfPizzas, productName);
+    OrderResponse response = mario.order(request);
+    BigDecimal expectedTotalPrice = Jambon.PRICE.multiply(new BigDecimal(numberOfPizzas));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
