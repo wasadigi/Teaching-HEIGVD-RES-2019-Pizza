38efc33fcb3205a6c66623f7e47d615865f3d6c3
luca de Carvalho <luca.de.carvalho@gmail.com>
2019-03-04 10:34:24 +0100

    Added and tested Hawai

---
+++ src/test/java/ch/heigvd/res/pizza/domain/goturak/HawaiTest.java
@@ -0,0 +1,34 @@
+package ch.heigvd.res.pizza.domain.goturak;
+
+import ch.heigvd.res.pizza.domain.Pizzaiolo;
+import ch.heigvd.res.pizza.domain.goturak.Hawai;
+import ch.heigvd.res.pizza.domain.wasadigi.Margherita;
+import ch.heigvd.res.pizza.protocol.OrderRequest;
+import ch.heigvd.res.pizza.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HawaiTest {
+
+  @Test
+  void thePriceAndNameForHawaiShouldBeCorrect() {
+    Hawai pizza = new Hawai();
+    assertEquals(pizza.getName(), Hawai.NAME);
+    assertEquals(pizza.getPrice(), Hawai.PRICE);
+  }
+
+  @Test
+  void aPizzaioloShouldAcceptAnOrderForHawai() {
+    Pizzaiolo mario = new Pizzaiolo();
+    String productName = "ch.heigvd.res.pizza.domain.goturak.Hawai";
+    int numberOfPizzas = 2;
+    OrderRequest request = new OrderRequest(numberOfPizzas, productName);
+    OrderResponse response = mario.order(request);
+    BigDecimal expectedTotalPrice = Hawai.PRICE.multiply(new BigDecimal(numberOfPizzas));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
