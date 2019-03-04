4b1f20f80cad46176a76e22d3ce6d9a497211cad
UncleBen420 <remyvu@outlook.com>
2019-03-04 10:41:45 +0100

    Ananas

---
+++ src/test/java/ch/heigvd/res/pizza/domain/Remy/AnanasTest.java
@@ -0,0 +1,32 @@
+package ch.heigvd.res.pizza.domain.Remy;
+
+import ch.heigvd.res.pizza.domain.Pizzaiolo;
+import ch.heigvd.res.pizza.protocol.OrderRequest;
+import ch.heigvd.res.pizza.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MargheritaTest {
+
+  @Test
+  void thePriceAndNameForAnanasShouldBeCorrect() {
+    Ananas beer = new Ananas();
+    assertEquals(beer.getName(), Ananas.NAME);
+    assertEquals(beer.getPrice(), Ananas.PRICE);
+  }
+
+  @Test
+  void aPizzaioloShouldAcceptAnOrderForAnanas() {
+    Pizzaiolo mario = new Pizzaiolo();
+    String productName = "ch.heigvd.res.pizza.domain.Remy.Ananas";
+    int numberOfPizzas = 2;
+    OrderRequest request = new OrderRequest(numberOfPizzas, productName);
+    OrderResponse response = mario.order(request);
+    BigDecimal expectedTotalPrice = Ananas.PRICE.multiply(new BigDecimal(numberOfPizzas));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
