02085a37826b75ce2b44d60b93cbb7dfa84ede1a
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2019-03-04 10:49:20 +0100

    Fix name and add static

--- src/main/java/ch/heigvd/res/pizza/domain/jzaehrin/Hawai.java
+++ src/main/java/ch/heigvd/res/pizza/domain/jzaehrin/Hawai.java
@@ -2,16 +2,18 @@ package ch.heigvd.res.pizza.domain.jzaerin;
 
 import ch.heigvd.res.pizza.domain.wasadigi.IProduct;
 
-
 import java.math.BigDecimal;
 
-public class Calabres implements IProduct {
+public class Hawai implements IProduct {
+
+    public final static String NAME = "Hawai Pizza";
+    public final static BigDecimal PRICE = new BigDecimal(15);
 
     public String getName() {
-        return "Hawai Pizza";
+        return NAME;
     }
 
     public BigDecimal getPrice(){
-        return 15;
+        return PRICE;
     }
 }
