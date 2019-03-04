68594c2c750f9ab94d4151837ddc013f8a0ab57f
Julien Quartier <julien.quartier@bluewin.ch>
2019-03-04 10:34:28 +0100

    Adding blue pizza

---
+++ src/test/java/ch/heigvd/res/pizza/domain/briskocube/BlueTest.java
@@ -0,0 +1,33 @@
+package ch.heigvd.res.pizza.domain.briskocube;
+
+import ch.heigvd.res.pizza.domain.Pizzaiolo;
+import ch.heigvd.res.pizza.domain.briskocube.Blue;
+import ch.heigvd.res.pizza.protocol.OrderRequest;
+import ch.heigvd.res.pizza.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BlueTest {
+
+    @Test
+    void thePriceAndNameForMargheritaShouldBeCorrect() {
+        Blue beer = new Blue();
+        assertEquals(beer.getName(), Blue.NAME);
+        assertEquals(beer.getPrice(), Blue.PRICE);
+    }
+
+    @Test
+    void aPizzaioloShouldAcceptAnOrderForMargherita() {
+        Pizzaiolo mario = new Pizzaiolo();
+        String productName = "ch.heigvd.res.pizza.domain.briskocube.Blue";
+        int numberOfPizzas = 2;
+        OrderRequest request = new OrderRequest(numberOfPizzas, productName);
+        OrderResponse response = mario.order(request);
+        BigDecimal expectedTotalPrice = Blue.PRICE.multiply(new BigDecimal(numberOfPizzas));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
+}
